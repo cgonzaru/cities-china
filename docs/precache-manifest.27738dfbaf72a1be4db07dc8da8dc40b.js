5c453cfb44208e7e7bb011993912828328fdd7ca
cgonzaru <cristina.glez.rubio@gmail.com>
2019-11-27 14:51:20 +0100

    added docs

---
+++ docs/precache-manifest.27738dfbaf72a1be4db07dc8da8dc40b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be34cb87ba3c2db2d048e9f3e43fc1bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1004d6dfe9483766e650",
+    "url": "./static/css/main.7f6002c2.chunk.css"
+  },
+  {
+    "revision": "5a6abcce655e0cdf8075",
+    "url": "./static/js/2.6c67e37a.chunk.js"
+  },
+  {
+    "revision": "1004d6dfe9483766e650",
+    "url": "./static/js/main.5bb2adbf.chunk.js"
+  },
+  {
+    "revision": "a6217312e436eea6372f",
+    "url": "./static/js/runtime-main.3fdacaa6.js"
+  },
+  {
+    "revision": "3cd6e3834cc0e40e1808c770a72b9768",
+    "url": "./static/media/mountains.3cd6e383.png"
+  }
+]);
